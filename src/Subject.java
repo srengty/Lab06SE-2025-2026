3ffd8891edea11462071e498508606d39930aef9
srengty <sreng.itc@gmail.com>
2025-12-25 08:57:18 +0700

    add java doc

--- src/Subject.java
+++ src/Subject.java
@@ -12,8 +12,19 @@ public class Subject{
     private String name;
     private int totalCredits = 1;
     private int totalHours = 50;
+    public static final double MAX_ABSENCE_SCORE = 10;
     public String toString(){
         return String.format("%s (%d credits, %d hours)",name, totalCredits, totalHours);
+    }
+    /**
+     * Find the scores of absences.
+     * @param absences number of absences
+     * @return absence score
+     */
+    public double calculateAbsencesScore(int absences){
+        // if totalHours = 10, absences = 2 => score=8
+        // if totalHours = 64, absences = 10 => score=?
+        return MAX_ABSENCE_SCORE - (10.0*absences/totalHours);
     }
     public static void main(String[] args) {
         Subject sb = new Subject();
@@ -21,6 +32,6 @@ public class Subject{
         sb.totalCredits = 3;
         sb.totalHours = 64;
         System.out.println(sb);
-
+        System.out.println("Absence score: "+ sb.calculateAbsencesScore(32));
     }
 }
